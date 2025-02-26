0baed243d0b3020e1bf3f71a7c1e9e3443dbd365
Tran Minh Tri <minhtri084038@gmail.com>
2025-02-26 23:33:37 +0700

    Hoàn thành bài 1

--- react_tuan_3/src/components/Footer.jsx
+++ react_tuan_3/src/components/Footer.jsx
@@ -1,9 +1,51 @@
+import '../components/footer.css'
+import img from '../assets/chefifywhite.png';
+
 function Footer(){
     return (
         <>
            <div className="footer">
-            <div className="innerLeft"></div>
-            <div className="innerRight"></div>
+            <div className="innerLeft">
+                <h4>About us</h4>
+                <p>Welcome to our website, a wonderful place to explore and learn how to cook like a pro</p>
+                <div className='search'>
+                    <input type="text" placeholder='Enter your email'/>
+                    <button>Send</button>
+                </div>
+                <div className='box chefify-box'>
+                    <img src={img} alt="" />
+                    <li>2023 Chefify Company</li>
+                    <li>Terms of Servicel Privacy Policy</li>
+                </div>
+            </div>
+            <div className="innerCenter">
+                <div className='learn box'>
+                    <h4>Learn more</h4>
+                    <li>Our Cooks</li>
+                    <li>See Our Features</li>
+                    <li>FAQ</li>
+                </div>
+
+                <div className='shop box'>
+                    <h4>Learn more</h4>
+                    <li>Our Cooks</li>
+                    <li>See Our Features</li>
+                    <li>FAQ</li>
+                </div>
+
+            </div>
+            <div className="innerRight">
+            <div className='recipes box'>
+                    <h4>Recipes</h4>
+                    <li>What to Cook This Week</li>
+                    <li>Pasta</li>
+                    <li>Dinner</li>
+                    <li>Healthy</li>
+                    <li>Vegetarian</li>
+                    <li>Vegan</li>
+                    <li>Chirismas</li>
+                </div>
+            </div>
            </div>
         </>
     )
